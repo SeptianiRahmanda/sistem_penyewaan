01f104109010307ac4a257af634281f4fe5de4c3
SeptianiRahmanda <septianirahmanda317@gmail.com>
2026-09-11 09:28:41 +0800

    Create room list page

---
+++ resources/views/rooms/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Kamar</title>
+</head>
+<body>
+
+    <h1>Data Kamar</h1>
+
+    <p>Halaman data kamar berhasil dibuat.</p>
+
+</body>
+</html>
